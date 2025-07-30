9ecb048dc471dda3569d2f977651975bd3305ca5
tomscollard <tomscollard@outlook.com>
2025-07-30 20:56:00 +0100

    made movement prototype
    :grin:

---
+++ GMTK Parent Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float sprintSpeedMult = 1.5f;
+    public float acceleration = 10f;
+    public float deceleration = 8f;
+
+    private Rigidbody2D rb;
+    private Vector2 inputDirection;
+    private Vector2 currentVelocity;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float directionX = Input.GetAxisRaw("Horizontal");
+        float directionY = Input.GetAxisRaw("Vertical");
+
+        inputDirection = new Vector2(directionX, directionY).normalized;
+
+        float speed = moveSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed *= sprintSpeedMult;
+        }
+
+        Vector2 targetVelocity = inputDirection * speed;
+        float lerpSpeed;
+
+        if (inputDirection.magnitude > 0)
+        {
+            lerpSpeed = acceleration;
+        }
+        else
+        {
+            lerpSpeed = deceleration;
+        }
+
+        currentVelocity = Vector2.Lerp(currentVelocity, targetVelocity, lerpSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + currentVelocity * Time.fixedDeltaTime);
+    }
+}
